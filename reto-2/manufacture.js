b325aad5c4657067656feac6fa7ef582b5737ed4
Isaac Leonardo M <mcisaacleonardo@gmail.com>
2023-12-04 10:49:39 -0400

    Refactoring code to make it more readable

--- reto-2/manufacture.js
+++ reto-2/manufacture.js
@@ -1,20 +1,5 @@
 export function manufacture(gifts, materials) {
-  const result = []
-
-  for (const gift of gifts) {
-    for (const characterIndex in gift) {
-      // Don't add it to result
-      if (!materials.includes(gift[characterIndex]))
-        break
-      
-      // Keep evaluating
-      if (characterIndex != gift.length - 1)
-        continue
-
-      // Add it to result
-      result.push(gift)
-    }
-  }
-
-  return result
+  return gifts.filter(
+    gift => gift.split('').every(character => materials.includes(character))
+  );
 }
